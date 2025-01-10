7f1c3751303f7738ce4d05c3313120145d7ea4ef
frilip <rfilippos@proton.me>
2025-01-10 17:47:28 +0200

    EDGES GAMMA RELATION CONFIRMED BABY

--- distance_newman_test.py
+++ distance_newman_test.py
@@ -7,7 +7,7 @@ import matplotlib.colors as mcolors
 from color_communites import color_communities
 
 
-
+'''
 
 # ------------- TEST -----------------
 G = nx.Graph()
@@ -60,20 +60,23 @@ comm = newman_greedy_distance(G, gamma)
 print(comm)
 color_communities(G, comm)
 plt.savefig('./plots/nonconnected'+str(gamma)+'.pdf')
+'''
+
+
 
 K1 = generate_full_cluster_graph_same_size(1,4)
 K2 = generate_full_cluster_graph_same_size(1,6)
-K3 = generate_full_cluster_graph_same_size(1,10)
-K4 = generate_full_cluster_graph_same_size(1,20)
+K3 = generate_full_cluster_graph_same_size(1,40)
+K4 = generate_full_cluster_graph_same_size(1,100)
 K = nx.disjoint_union(K1, K2)
 K.add_edge(3,4)
 K = nx.disjoint_union(K, K3)
 K.add_edge(9,10)
 K = nx.disjoint_union(K,K4)
-K.add_edge(19,20)
-K.add_edge(21,0)
+K.add_edge(49,50)
+K.add_edge(149,0)
 
-gamma = 0.005
+gamma = 0.0003
 comm = newman_greedy_distance(K, gamma)
 color_communities(K, comm)
 plt.savefig('./plots/different_size_communities.pdf')
